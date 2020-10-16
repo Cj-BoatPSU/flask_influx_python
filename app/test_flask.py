81f7346f2bfb0742f9e18eda7c05831145ad53ec
CjBoat2542 <55519892+CjBoat2542@users.noreply.github.com>
2020-10-16 21:57:24 +0700

    first commit

---
+++ app/test_flask.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, Response
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+@app.route('/test')
+def output():
+	# serve index template
+	return render_template('test1.html')
+
+if __name__ == '__main__':
+    app.run()
